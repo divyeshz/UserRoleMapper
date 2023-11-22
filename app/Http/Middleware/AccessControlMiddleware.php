99bee0bf366c0b695fabdadb55b92d737be31779
divyeshz <divyeshm@zignuts.com>
2023-11-22 11:45:28 +0530

    Refactor AccessControlMiddleware to use module and action parameter for route permission checks

--- app/Http/Middleware/AccessControlMiddleware.php
+++ app/Http/Middleware/AccessControlMiddleware.php
@@ -14,45 +14,40 @@ class AccessControlMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $moduleName, $action): Response
     {
         $user = Auth::user();
 
-        $requestedRoute = explode(".", $request->route()->getName()); // Get requested route name
         $permissions = $user->roles->flatMap->permissions; // Get all permissions associated with user roles
-
-        if($requestedRoute[0] == 'dashboard' || $requestedRoute[0] == 'profile' ||  $requestedRoute[0] == 'changePassword'){
-            return $next($request);
-        }
 
         foreach ($permissions as $permission) {
             foreach ($permission->modules as $module) {
 
                 // User has permission for the requested route
-                if (strtolower($module['name']) === $requestedRoute['0']) {
+                if (strtolower($module['name']) === $moduleName) {
 
-                    // User has permission to view 'list' & 'status update' route
-                    if (($module->pivot['add_access'] || $module->pivot['edit_access'] || $module->pivot['delete_access'] || $module->pivot['view_access']) && ($requestedRoute['1'] == 'list' || $requestedRoute['1'] == 'status' || $requestedRoute['1'] == 'forceLogout')) {
+                    // User has permission to Access 'list' & 'status update' route
+                    if (($module->pivot['add_access'] || $module->pivot['edit_access'] || $module->pivot['delete_access'] || $module->pivot['view_access']) && ($action == 'list' || $action == 'status' || $action == 'forceLogout')) {
                         return $next($request);
                     }
 
-                    // User has permission to view 'add' route
-                    if ($module->pivot['add_access'] && ($requestedRoute['1'] == 'addForm' || $requestedRoute['1'] == 'store')) {
+                    // User has permission to Access 'add' route
+                    if ($module->pivot['add_access'] && ($action == 'add')) {
                         return $next($request);
                     }
 
-                    // User has permission to view 'edit' route
-                    if ($module->pivot['edit_access'] && ($requestedRoute['1'] == 'editForm' || $requestedRoute['1'] == 'update')) {
+                    // User has permission to Access 'edit' route
+                    if ($module->pivot['edit_access'] && ($action == 'edit')) {
                         return $next($request);
                     }
 
-                    // User has permission to view 'user.list' route
-                    if ($module->pivot['view_access'] && ($requestedRoute['1'] == 'show')) {
+                    // User has permission to Access 'view' route
+                    if ($module->pivot['view_access'] && ($action == 'view')) {
                         return $next($request);
                     }
 
-                    // User has permission to view 'user.list' route
-                    if ($module->pivot['delete_access'] && ($requestedRoute['1'] == 'destroy' || $requestedRoute['1'] == 'delete' || $requestedRoute['1'] == 'restore')) {
+                    // User has permission to Access 'soft delete', 'hard delete' & 'restore' route
+                    if ($module->pivot['delete_access'] && ($action == 'delete' || $action == 'restore' )) {
                         return $next($request);
                     }
                 }
--- routes/web.php
+++ routes/web.php
@@ -39,67 +39,66 @@ Route::controller(AuthController::class)->group(function () {
     });
 });
 
-Route::group(['middleware' => ['auth', 'forceLogout','access.control']], function () {
+Route::group(['middleware' => ['auth', 'forceLogout']], function () {
 
     // User Routes Group
     Route::controller(UserController::class)->group(function () {
-        Route::get('profile', 'profile')->name('profile');
-
         Route::prefix('user')->group(function () {
-            Route::get('list', 'index')->name('user.list');
-            Route::get('create', 'create')->name('user.addForm');
-            Route::post('store', 'store')->name('user.store');
-            Route::get('show/{id}', 'show')->name('user.show');
-            Route::get('edit/{id}', 'edit')->name('user.editForm');
-            Route::post('update/{id}', 'update')->name('user.update');
-            Route::post('destroy/{id}', 'destroy')->name('user.destroy');
-            Route::post('delete/{id}', 'delete')->name('user.delete');
-            Route::get('restore/{id}', 'restore')->name('user.restore');
-            Route::post('status', 'status')->name('user.status');
-            Route::post('logout/{id}', 'forceLogout')->name('user.forceLogout');
+            Route::get('profile', 'profile')->name('profile');
+            Route::get('list', 'index')->name('user.list')->middleware('access.control:user,list');
+            Route::get('create', 'create')->name('user.addForm')->middleware('access.control:user,add');
+            Route::post('store', 'store')->name('user.store')->middleware('access.control:user,add');
+            Route::get('show/{id}', 'show')->name('user.show')->middleware('access.control:user,view');
+            Route::get('edit/{id}', 'edit')->name('user.editForm')->middleware('access.control:user,edit');
+            Route::post('update/{id}', 'update')->name('user.update')->middleware('access.control:user,edit');
+            Route::post('destroy/{id}', 'destroy')->name('user.destroy')->middleware('access.control:user,delete');
+            Route::post('delete/{id}', 'delete')->name('user.delete')->middleware('access.control:user,delete');
+            Route::get('restore/{id}', 'restore')->name('user.restore')->middleware('access.control:user,restore');
+            Route::post('status', 'status')->name('user.status')->middleware('access.control:user,status');
+            Route::post('logout/{id}', 'forceLogout')->name('user.forceLogout')->middleware('access.control:user,forceLogout');
         });
     });
 
     // Role Routes Group
     Route::controller(RoleController::class)->prefix('role')->group(function () {
-        Route::get('list', 'index')->name('role.list');
-        Route::get('create', 'create')->name('role.addForm');
-        Route::post('store', 'store')->name('role.store');
-        Route::get('show/{id}', 'show')->name('role.show');
-        Route::get('edit/{id}', 'edit')->name('role.editForm');
-        Route::post('update/{id}', 'update')->name('role.update');
-        Route::post('destroy/{id}', 'destroy')->name('role.destroy');
-        Route::post('delete/{id}', 'delete')->name('role.delete');
-        Route::get('restore/{id}', 'restore')->name('role.restore');
-        Route::post('status', 'status')->name('role.status');
+        Route::get('list', 'index')->name('role.list')->middleware('access.control:role,list');
+        Route::get('create', 'create')->name('role.addForm')->middleware('access.control:role,add');
+        Route::post('store', 'store')->name('role.store')->middleware('access.control:role,add');
+        Route::get('show/{id}', 'show')->name('role.show')->middleware('access.control:role,view');
+        Route::get('edit/{id}', 'edit')->name('role.editForm')->middleware('access.control:role,edit');
+        Route::post('update/{id}', 'update')->name('role.update')->middleware('access.control:role,edit');
+        Route::post('destroy/{id}', 'destroy')->name('role.destroy')->middleware('access.control:role,delete');
+        Route::post('delete/{id}', 'delete')->name('role.delete')->middleware('access.control:role,delete');
+        Route::get('restore/{id}', 'restore')->name('role.restore')->middleware('access.control:role,restore');
+        Route::post('status', 'status')->name('role.status')->middleware('access.control:role,status');
     });
 
     // Permission Routes Group
     Route::controller(PermissionController::class)->prefix('permission')->group(function () {
-        Route::get('list', 'index')->name('permission.list');
-        Route::get('create', 'create')->name('permission.addForm');
-        Route::post('store', 'store')->name('permission.store');
-        Route::get('show/{id}', 'show')->name('permission.show');
-        Route::get('edit/{id}', 'edit')->name('permission.editForm');
-        Route::post('update/{id}', 'update')->name('permission.update');
-        Route::post('destroy/{id}', 'destroy')->name('permission.destroy');
-        Route::post('delete/{id}', 'delete')->name('permission.delete');
-        Route::get('restore/{id}', 'restore')->name('permission.restore');
-        Route::post('status', 'status')->name('permission.status');
+        Route::get('list', 'index')->name('permission.list')->middleware('access.control:permission,list');
+        Route::get('create', 'create')->name('permission.addForm')->middleware('access.control:permission,add');
+        Route::post('store', 'store')->name('permission.store')->middleware('access.control:permission,add');
+        Route::get('show/{id}', 'show')->name('permission.show')->middleware('access.control:permission,view');
+        Route::get('edit/{id}', 'edit')->name('permission.editForm')->middleware('access.control:permission,edit');
+        Route::post('update/{id}', 'update')->name('permission.update')->middleware('access.control:permission,edit');
+        Route::post('destroy/{id}', 'destroy')->name('permission.destroy')->middleware('access.control:permission,delete');
+        Route::post('delete/{id}', 'delete')->name('permission.delete')->middleware('access.control:permission,delete');
+        Route::get('restore/{id}', 'restore')->name('permission.restore')->middleware('access.control:permission,restore');
+        Route::post('status', 'status')->name('permission.status')->middleware('access.control:permission,status');
     });
 
     // Module Routes Group
     Route::controller(ModuleController::class)->prefix('module')->group(function () {
-        Route::get('list', 'index')->name('module.list');
-        Route::get('create', 'create')->name('module.addForm');
-        Route::post('store', 'store')->name('module.store');
-        Route::get('show/{id}', 'show')->name('module.show');
-        Route::get('edit/{id}', 'edit')->name('module.editForm');
-        Route::post('update/{id}', 'update')->name('module.update');
-        Route::post('destroy/{id}', 'destroy')->name('module.destroy');
-        Route::post('delete/{id}', 'delete')->name('module.delete');
-        Route::get('restore/{id}', 'restore')->name('module.restore');
-        Route::post('status', 'status')->name('module.status');
+        Route::get('list', 'index')->name('module.list')->middleware('access.control:module,list');
+        Route::get('create', 'create')->name('module.addForm')->middleware('access.control:module,add');
+        Route::post('store', 'store')->name('module.store')->middleware('access.control:module,add');
+        Route::get('show/{id}', 'show')->name('module.show')->middleware('access.control:module,view');
+        Route::get('edit/{id}', 'edit')->name('module.editForm')->middleware('access.control:module,edit');
+        Route::post('update/{id}', 'update')->name('module.update')->middleware('access.control:module,edit');
+        Route::post('destroy/{id}', 'destroy')->name('module.destroy')->middleware('access.control:module,delete');
+        Route::post('delete/{id}', 'delete')->name('module.delete')->middleware('access.control:module,delete');
+        Route::get('restore/{id}', 'restore')->name('module.restore')->middleware('access.control:module,restore');
+        Route::post('status', 'status')->name('module.status')->middleware('access.control:module,status');
     });
 
     Route::get('dashboard', function () {
