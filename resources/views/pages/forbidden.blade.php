d7f31bffbe997bfa50330c787ba5a38c7b7f0e19
divyeshz <divyeshm@zignuts.com>
2023-11-22 12:21:34 +0530

    Refactor forbidden page styling and structure

--- resources/views/pages/forbidden.blade.php
+++ resources/views/pages/forbidden.blade.php
@@ -3,57 +3,48 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Access Denied - 403 Error</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            text-align: center;
-        }
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UserRoleMapper | 403</title>
 
-        .container {
-            background-color: #fff;
-            border-radius: 8px;
-            padding: 40px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
 
-        h1 {
-            margin-bottom: 20px;
-            color: #f44336;
-        }
+    <!-- core:css -->
+    <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+    <!-- endinject -->
 
-        p {
-            color: #555;
-        }
-
-        .back-button {
-            padding: 10px 20px;
-            background-color: #2196f3;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        .back-button:hover {
-            background-color: #0e81ce;
-        }
-    </style>
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/demo3/style.css') }}">
+    <!-- End layout styles -->
 </head>
 
 <body>
-    <div class="container">
-        <h1>Access Denied - 403 Error</h1>
-        <p>Oops! You don't have permission to access this page.</p>
-        <p><button onclick="window.history.go(-1);" class="back-button">Go Back</button> </p>
+    <div class="main-wrapper">
+        <div class="page-wrapper full-page">
+            <div class="page-content d-flex align-items-center justify-content-center">
+
+                <div class="row w-100 mx-0 auth-page">
+                    <div class="col-md-8 col-xl-6 mx-auto d-flex flex-column align-items-center">
+                        <img src="{{ asset('assets/images/others/404.svg') }}" class="img-fluid mb-2" alt="404">
+                        <h1 class="fw-bolder mb-22 mt-2 tx-80 text-muted">403</h1>
+                        <h4 class="mb-2">Access Denied</h4>
+                        <h6 class="text-muted mb-3 text-center">Oopps!! You don't have permission to access this page.</h6>
+                        <a href="{{ route('dashboard') }}" class="back-button">Back to home</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
+
+    <!-- core:js -->
+    <script src="{{ asset('assets/vendors/core/core.js') }}"></script>
+    <!-- endinject -->
+
 </body>
 
 </html>
