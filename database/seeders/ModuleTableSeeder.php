590ab32bbd124a6b6076c63f66fd74846c90fa21
divyeshz <divyeshm@zignuts.com>
2023-11-22 14:56:00 +0530

    Refactor ModuleTableSeeder to set parent_id for Account module

--- database/seeders/ModuleTableSeeder.php
+++ database/seeders/ModuleTableSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Module;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -14,26 +13,44 @@ class ModuleTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $accountModuleId = Str::uuid(); // Generate UUID for Account module
         Module::insert([
-            ['id'           =>  Str::uuid(),
-            'code'          => 'acc',
-            'name'          => 'Account',
-            'display_order' => 1,],
-
-            ['id'           =>  Str::uuid(),
-            'code'          => 'role',
-            'name'          => 'Role',
-            'display_order' => 2,],
-
-            ['id'           =>  Str::uuid(),
-            'code'          => 'perm',
-            'name'          => 'Permission',
-            'display_order' => 3,],
-
-            ['id'           => Str::uuid(),
-            'code'          => 'user',
-            'name'          => 'User',
-            'display_order' => 4,],
+            [
+                'id' => $accountModuleId,
+                'code' => 'acc',
+                'name' => 'Account',
+                'display_order' => 1,
+                'parent_id' => null,
+            ],
         ]);
+
+        $accountModule = Module::find($accountModuleId); // Retrieve the Account module
+
+        if ($accountModule) {
+            Module::insert([
+                [
+                    'id' => Str::uuid(),
+                    'code' => 'role',
+                    'name' => 'Role',
+                    'display_order' => 2,
+                    'parent_id' => $accountModuleId, // Assign Account module's ID as parent_id
+                ],
+                [
+                    'id' => Str::uuid(),
+                    'code' => 'perm',
+                    'name' => 'Permission',
+                    'display_order' => 3,
+                    'parent_id' => $accountModuleId, // Assign Account module's ID as parent_id
+                ],
+                [
+                    'id' => Str::uuid(),
+                    'code' => 'user',
+                    'name' => 'User',
+                    'display_order' => 4,
+                    'parent_id' => $accountModuleId, // Assign Account module's ID as parent_id
+                ],
+            ]);
+        }
     }
+
 }
